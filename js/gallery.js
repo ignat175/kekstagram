d4a74f96afa552b777deb053516b653e8f094a6b
Svetlana <sveta_ignatieva@mail.ru>
2022-09-24 13:24:32 +0300

    240922 Home Work

--- js/gallery.js
+++ js/gallery.js
@@ -16,8 +16,15 @@ const renderPictures = (pictures) => {
     while (i < pictures.length) {
         const picture = pictures[i];
 
-        const p = pictureTemplate.cloneNode(true);  
-        p.querySelector('img').setAttribute('src', picture.url);
+        const pictureClone = pictureTemplate.cloneNode(true);  
+        // pictureClone.querySelector('img').setAttribute('src', picture.url);
+        pictureClone.querySelector('picture__likes').textContent = picture.likes;
+        pictureClone.querySelector('picture__comments').textContent = picture.comments.length;
+
+        const imgElement = pictureClone.querySelector('img');
+        imgElement.setAttribute('src', picture.url);
+        imgElement.setAttribute('alt', picture.description);
+        
         // const html = `
         //     <a href="#" class="picture">
         //         <img
@@ -34,7 +41,7 @@ const renderPictures = (pictures) => {
         //     </a>
         // `;
    
-        picturesElement.insertAdjacentElement('beforeend', p);
+        picturesElement.insertAdjacentElement('beforeend', pictureClone);
         i++;
     }
 };
