6ee39998cdc679b87c0339143a27d8524aad5374
Svetlana <sveta_ignatieva@mail.ru>
2022-12-31 08:45:57 +0300

    final-2

--- client/src/filters.js
+++ client/src/filters.js
@@ -3,58 +3,64 @@ import {renderPictures} from "./gallery.js";
 
 const filtarsButtonElements = document.querySelectorAll('.img-filters__button');
 
+const Filter = {
+    DEFAULT: 'default',
+    RANDOM: 'random',
+    DISCUSSED: 'discussed'
+};
+
 const comparePictures = (pictureA, pictureB) => {
-   const a = pictureA.comments.length;
-   const b = pictureB.comments.length;
+    const a = pictureA.comments.length;
+    const b = pictureB.comments.length;
    
-   return b - a;
+    return b - a;
 };
 
 const getRandomPictures = (pictures) => {
-   const randomPictures = [];
-   let count = pictures.length;
+    const randomPictures = [];
+    let count = pictures.length;
 
-   if (pictures.length > 10) {
-       count = 10;
-   }
+    if (pictures.length > 10) {
+        count = 10;
+    }
 
-  while (randomPictures.length < count) {
-       const randomPicture = getRandomArrayElement(pictures);
+    while (randomPictures.length < count) {
+        const randomPicture = getRandomArrayElement(pictures);
 
-       if (!randomPictures.find((picture) => picture.id === randomPicture.id)) {
-           randomPictures.push(randomPicture);
-       }
-   }
+        if (!randomPictures.find((picture) => picture.id === randomPicture.id)) {
+            randomPictures.push(randomPicture);
+        }
+    }
 
-   return randomPictures;
+    return randomPictures;
 };
 
 const onFilterClick = (evt, pictures) => {
-   if (evt.target.matches('.img-filters__button')) {
-       const filter = evt.target.id.split('-')[1];
-       if (onFilterClick.filter !== 'random' && onFilterClick.filter === filter) {
-           return;
-       }
+    if (evt.target.matches('.img-filters__button')) {
+        const filter = evt.target.id.split('-')[1];
+        if (onFilterClick.filter === filter && filter !== Filter.RANDOM) {
+            return;
+        }
 
-       for (const button of filtarsButtonElements) {
-           button.classList.remove('img-filters__button--active');
-       }
+        for (const button of filtarsButtonElements) {
+            button.classList.remove('img-filters__button--active');
+        }
 
-       evt.target.classList.add('img-filters__button--active');
-       onFilterClick.filter = filter;
+        evt.target.classList.add('img-filters__button--active');
+        onFilterClick.filter = filter;
 
-       switch (filter) {
-           case 'default':
-               renderPictures(pictures, true);
-               break;
-           case 'random':
-               renderPictures(getRandomPictures(pictures), true);
-               break;
-           case 'discussed':
-               renderPictures(pictures.slice().sort(comparePictures), true);
-               break;
-       }
-   }
+        switch (filter) {
+            case Filter.DEFAULT:
+                renderPictures(pictures, true);
+                break;
+            case Filter.RANDOM:
+                renderPictures(getRandomPictures(pictures), true);
+                break;
+            case Filter.DISCUSSED:
+                renderPictures(pictures.slice().sort(comparePictures), true);
+                break;
+        }
+    }
 };
 
 export {onFilterClick};
